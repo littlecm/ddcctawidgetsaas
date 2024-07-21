b1cde9e4b1ead4b102c832cdf6b7f44bd78340e9
littlecm <clittle.norml@gmail.com>
2024-07-21 13:59:43 -0400

    Update leadForm.js

--- leadForm.js
+++ leadForm.js
@@ -7,13 +7,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const formData = new FormData(leadForm);
         const selectedQuestions = formData.getAll('questions').join(', ');
+        const firstName = formData.get('first_name');
+        const lastName = formData.get('last_name');
+        const phone = formData.get('phone');
         const email = formData.get('email');
         const accountId = document.getElementById('dealership-logo').dataset.accountId;
+        const vehicleDetails = JSON.parse(leadForm.dataset.vehicleDetails);
 
         const leadData = {
             account_id: accountId,
+            first_name: firstName,
+            last_name: lastName,
+            phone: phone,
             email: email,
             questions: selectedQuestions,
+            vehicle_title: vehicleDetails.title,
+            vehicle_price: vehicleDetails.price,
+            vehicle_vin: vehicleDetails.vin,
+            vehicle_stock_number: vehicleDetails.stockNumber,
+            vehicle_link: vehicleDetails.link,
             submitted_at: new Date().toISOString()
         };
 
@@ -29,9 +41,16 @@ document.addEventListener("DOMContentLoaded", function() {
                       mutation insert_cta_widget_lead_submissions_one($object: cta_widget_lead_submissions_insert_input!) {
                         insert_cta_widget_lead_submissions_one(object: $object) {
                           account_id
+                          first_name
+                          last_name
+                          phone
                           email
-                          id
                           questions
+                          vehicle_title
+                          vehicle_price
+                          vehicle_vin
+                          vehicle_stock_number
+                          vehicle_link
                           submitted_at
                         }
                       }
